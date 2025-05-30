2589543a02428c824f3da3bb8858fd478f78e44e
kiranbg316 <kiranbgode@gmail.com>
2025-05-30 15:34:38 +0530

    added echoserver and echoclient both are interacting

--- day1/FileReadWrite.java
+++ day1/FileReadWrite.java
@@ -2,17 +2,22 @@ import java.io.*;
 
 public class FileReadWrite {
     public static void main(String[] args) {
-       try(BufferedReader reader = new BufferedReader(new FileReader("day1/input.txt"))) {
+       try(
+               BufferedReader reader = new BufferedReader(new FileReader("day1/input.txt")) ;
+           BufferedWriter writer = new BufferedWriter(new FileWriter("day1/output.txt"));
+       ){
            String line;
            while ((line= reader.readLine()) != null){
-               System.out.println(line);
+           //    System.out.println(line);
+               writer.write(line);
+               writer.newLine();
 
            }
-           System.out.println("file read succesfully");
+           System.out.println("file read and written succesfully");
 
 
        }catch (IOException e){
-           System.out.println("error reading the file : ");
+           System.err.println("error during file operation:");
            e.printStackTrace();
        }
 
